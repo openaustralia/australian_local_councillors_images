92815fbab936125bd5dd6c7c178af1207e392f23
Henare Degan <henare.degan@gmail.com>
2016-04-19 18:52:05 +1000

    Move all env variables to .env, add example, support multiple files
    Not tested - yolo coding for the moment

--- scraper.rb
+++ scraper.rb
@@ -3,32 +3,31 @@ require "json"
 require "open-uri"
 require "fog"
 
-EVERYPOLITICIAN_URL = "https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/Ukraine/Verkhovna_Rada/ep-popolo-v1.0.json"
-S3_BUCKET = "ukraine-verkhovna-rada-deputy-images"
-
 s3_connection = Fog::Storage.new(
   provider: "AWS",
   aws_access_key_id: ENV["MORPH_AWS_ACCESS_KEY_ID"],
   aws_secret_access_key: ENV["MORPH_AWS_SECRET_ACCESS_KEY"]
 )
-directory = s3_connection.directories.get(S3_BUCKET)
+directory = s3_connection.directories.get(MORPH_S3_BUCKET)
 
-people = JSON.parse(open(EVERYPOLITICIAN_URL).read)["persons"]
+ENV["MORPH_POPOLO_URLS"].split.each do |url|
+  people = JSON.parse(open(url).read)["persons"]
 
-people.each do |person|
-  rada_id = person["identifiers"].find { |i| i["scheme"] == "rada" }["identifier"]
-  file_name = "#{rada_id}.jpg"
-  s3_url = "https://s3.amazonaws.com/ukraine-verkhovna-rada-deputy-images/#{file_name}"
+  people.each do |person|
+    rada_id = person["identifiers"].find { |i| i["scheme"] == "rada" }["identifier"]
+    file_name = "#{rada_id}.jpg"
+    s3_url = "https://s3.amazonaws.com/ukraine-verkhovna-rada-deputy-images/#{file_name}"
 
-  if ENV["MORPH_CLOBBER"] == "true" || directory.files.head(file_name).nil?
-    puts "Saving #{s3_url}"
-    directory.files.create(
-      key: file_name,
-      body: open(person["image"]).read,
-      public: true
-    )
-  else
-    puts "Skipping already saved #{s3_url}"
+    if ENV["MORPH_CLOBBER"] == "true" || directory.files.head(file_name).nil?
+      puts "Saving #{s3_url}"
+      directory.files.create(
+        key: file_name,
+        body: open(person["image"]).read,
+        public: true
+      )
+    else
+      puts "Skipping already saved #{s3_url}"
+    end
   end
 end
 
