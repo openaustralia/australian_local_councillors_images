8bfe4f470789d50b249d9cff8f65d14d6d328589
Luke Bacon <luke@equivalentideas.com>
2016-05-05 14:54:57 +1000

    Extract repetition into method

--- scraper.rb
+++ scraper.rb
@@ -12,6 +12,17 @@ def s3_url(path)
   "https://#{ENV['MORPH_S3_BUCKET']}.s3.amazonaws.com/#{path}"
 end
 
+def fetch_and_save_image(agent, directory, source_url, file_name)
+  puts "Fetching #{source_url}"
+  file_body = agent.get(source_url).body
+  puts "Saving image to #{s3_url(file_name)}"
+  directory.files.create(
+    key: file_name,
+    body: file_body,
+    public: true
+  )
+end
+
 # TODO: Make the url, width and height configurable with ENV variables
 def image_proccessing_proxy_url(image_source_url)
   "http://floating-refuge-38180.herokuapp.com/" + url_encode(image_source_url) + "/80/88.jpg"
@@ -62,26 +73,17 @@ popolo_urls.each do |url|
     resized_file_name = "#{person.id}-80x88.jpg"
 
     if ENV["MORPH_CLOBBER"] == "true" || directory.files.head(file_name).nil?
-      puts "Fetching #{person.image}"
-      image = agent.get(person.image).body
-      puts "Saving #{s3_url(file_name)}"
-      directory.files.create(
-        key: file_name,
-        body: image,
-        public: true
-      )
+      fetch_and_save_image(agent, directory, person.image, file_name)
     else
       puts "Skipping already saved #{s3_url(file_name)}"
     end
 
     if ENV["MORPH_CLOBBER_PROCESSED_IMAGES"] == "true" || directory.files.head(resized_file_name).nil?
-      puts "Fetching #{image_proccessing_proxy_url(s3_url(file_name))}"
-      resized_image = agent.get(image_proccessing_proxy_url(s3_url(file_name))).body
-      puts "Saving resized image to #{s3_url(resized_file_name)}"
-      directory.files.create(
-        key: resized_file_name,
-        body: resized_image,
-        public: true
+      fetch_and_save_image(
+        agent,
+        directory,
+        image_proccessing_proxy_url(s3_url(file_name)),
+        resized_file_name
       )
     else
       puts "Skipping already saved resized image #{s3_url(resized_file_name)}"
